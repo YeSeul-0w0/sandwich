fd0aff393b09d5e3056e1d7e389cd0e647319d8f
YeSeul-0w0 <choi333ys@gmail.com>
2022-12-06 09:51:48 +0900

    memo logic edit

--- src/calendar/memo.js
+++ src/calendar/memo.js
@@ -1,7 +1,7 @@
 export function memo() {
     const memo_info = document.getElementById('memo')
 
-    const save_list=[]
+    let save_list=[]
     const memo_list = localStorage.getItem('memo')
     if (memo_list) {
         let array = JSON.parse(memo_list)
